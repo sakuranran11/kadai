941a254f5c6eeb63b2b37312f69b6a2d806dd7ae
sakuranran11 <happy.sakuranran@gmail.com>
2019-10-23 05:16:34 +0000

    Some fix

---
+++ kaadai-2/index.js
@@ -0,0 +1,17 @@
+const name = 'sakura'
+console.log(name)
+const name = 'daigo'
+const name1 = 'sakura'
+name1 = 'daigo'
+let name2 ='rui'
+name2 = 'kasa'
+
+
+const name = 'sakura'
+console.log(typeof name)
+
+const firstName = 'Daigo'
+const secondName= 'Nishihata'
+console.log(firstName + secondName)
+
+
---
+++ kadai-3/index.js
@@ -0,0 +1,10 @@
+const firstName = 'Daigo'
+const secondName = 'Nishihata'
+console.log(firstName + secondName)
+const sum = 1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+ const hasDog = false
+ const hasCat = true
+ console.log(hasDog || hasCat)
+ console.log(hasDog && hasCat)
+console.log(!hasDog && !hasCat)
